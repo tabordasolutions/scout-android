039d9a119c7ef364b2912d154056080211874bb8
Luis Gutierrez <lgutierrez4435@hotmail.com>
2017-10-17 15:02:03 -0700

    Renamed all but one instance of "General Message" to "Field Report" as per OES-549

--- SCOUTMobile/SCOUT_Mobile/src/main/java/scout/edu/mit/ll/nics/android/fragments/OverviewFragment.java
+++ SCOUTMobile/SCOUT_Mobile/src/main/java/scout/edu/mit/ll/nics/android/fragments/OverviewFragment.java
@@ -599,26 +599,29 @@ public class OverviewFragment extends Fragment {
 		
 		@Override
 		public void onClick(View v) {
+
 			mDialogBuilder.setTitle(R.string.select_report_type);
+
 			mDialogBuilder.setMessage(R.string.feature_coming_soon);
 			mDialogBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-				
+
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					dialog.dismiss();
 				}
 			});
-		    
-//		    for(int i = 0; i < activeReports.size(); i++) {
-//		    	activeReports.set(i, activeReports.get(i).replace(mDataManager.getActiveIncidentName() + "-", ""));
-//		    }
-//		    
-//		    if(activeReports.size() == 0)
-//		    {
-//		    	activeReports.add(getString(R.string.reports_not_available_for_your_organization));
-//		    }
-//
-//			mDialogBuilder.setItems(activeReports.toArray(new String[activeReports.size()]), reportSelected);
+
+			//These lines enable the reports dialog menu
+		    /*for(int i = 0; i < activeReports.size(); i++) {
+		    	activeReports.set(i, activeReports.get(i).replace(mDataManager.getActiveIncidentName() + "-", ""));
+		    }
+
+		    if(activeReports.size() == 0)
+		    {
+		    	activeReports.add(getString(R.string.reports_not_available_for_your_organization));
+		    }*/
+
+			mDialogBuilder.setItems(activeReports.toArray(new String[activeReports.size()]), reportSelected);
 			mReportPopupMenu = mDialogBuilder.create();
 			
 		    mReportPopupMenu.show();
